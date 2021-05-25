2c725a29aad36668e90bbb9d4fdf5b06c0423207
Jose Espejo <espejor@gmail.com>
2021-05-25 22:53:25 +0200

    app.get('', function(req,res) {

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use('/api/operations',routes)
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+app.get('', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
